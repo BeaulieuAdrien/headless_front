5996b356450bda5eb11bc3fc9739f2ba47913745
ws_abeaulieu <adrien.beaulieu4@orange.fr>
2023-12-29 13:45:02 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1 class="title-header">Donnez votre avis sur des boutiques</h1>
+      <hr>
+    </div>
+  </header>
+
+  <RouterView />
+  
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import axios from 'axios';
+import {onMounted, ref} from "vue";
+
+const boutiques = ref([])
+
+onMounted(async () => {
+    try {
+        const response = await axios.get('http://localhost:1337/api/shops')
+        boutiques.value = response.data
+        console.log(boutiques)
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+</script>
+
+<template>
+  <main>
+      <pre>{{ boutiques.data }}</pre>
+    <div v-for="boutique in boutiques.data" :key="boutique.id" class="boutique">
+        <h2>{{ boutique.attributes.name }}</h2>
+        <p>{{ boutique.attributes.description }}</p>
+        <p>Nombre de commentaires : 5</p>
+        <button>Consulter</button>
+    </div>
+  </main>
+</template>
