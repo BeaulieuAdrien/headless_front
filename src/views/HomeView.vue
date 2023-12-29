07a6e2411ec874cc3634a4a70aadabae19022f93
ws_abeaulieu <adrien.beaulieu4@orange.fr>
2023-12-29 14:48:11 +0100

    Display list of shops in home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ const boutiques = ref([])
 
 onMounted(async () => {
     try {
-        const response = await axios.get('http://localhost:1337/api/shops')
+        const response = await axios.get('http://localhost:1337/api/shops?populate=comments')
         boutiques.value = response.data
         console.log(boutiques)
     } catch (error) {
@@ -17,12 +17,15 @@ onMounted(async () => {
 
 <template>
   <main>
-      <pre>{{ boutiques.data }}</pre>
-    <div v-for="boutique in boutiques.data" :key="boutique.id" class="boutique">
+      <pre>{{ boutiques }}</pre>
+      <div v-for="boutique in boutiques.data" :key="boutique.id" class="boutique">
         <h2>{{ boutique.attributes.name }}</h2>
-        <p>{{ boutique.attributes.description }}</p>
-        <p>Nombre de commentaires : 5</p>
-        <button>Consulter</button>
+        <p class="description">{{ boutique.attributes.description }}</p>
+        <p>{{ boutique.attributes.address }}</p>
+        <p>Nombre de commentaires : {{ boutique.attributes.comments.data.length }}</p>
+        <button>
+            <router-link :to="'/boutique/' + boutique.id">Consulter</router-link>
+        </button>
     </div>
   </main>
 </template>
